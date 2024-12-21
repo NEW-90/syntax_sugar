cbee5f11472b3db5022d2ac57bb75f1e992ed7f3
Sigma <dlscam112@gmail.com>
2024-12-22 03:45:14 +0500

    _currend

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
+//обьевляем асинхронную фнукцию
 async function i(){
     console.info("подаждите...")
+    //в блоке try - обрабатывается запрос
     try {
+        //отправляем запрос к серверу
         const res = await fetch('https://httpbin.org/status/200,500');
     
+    //если успешное соеденение и данные полчены то тогда выводим 'Good'
     if(res.ok){
         console.log("Good");
-    } else {
+    } 
+    //если ошибка то тогда выводим 'ERROR;
+    else {
         console.error("ERROR");
     }
     
+    //если сервер вернул ошибку то тогда запрос будет перехвачено блоком 'catch'
     } catch(error) {
         console.log('Error');
     }
 }
 
+//вызываем фнукцию
 i();
